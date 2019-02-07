bad0de56c5ea7f690c355a6aa24fdff57a030230
小島拓也 <kojimatakuya@TKMacBookAir.local>
2019-02-07 12:13:11 +0900

    change arguments

--- RouterBase.py
+++ RouterBase.py
@@ -9,20 +9,20 @@ class RouterBase(metaclass=ABCMeta):
 
     @staticmethod
     @abstractmethod
-    def comp_routing(CGRA, DFG, mapping, **info):
+    def comp_routing(CGRA, comp_DFG, mapping, routed_graph, **info):
         pass
 
     @staticmethod
     @abstractmethod
-    def const_routing(CGRA, DFG, **info):
+    def const_routing(CGRA, const_DFG, mapping, routed_graph, **info):
         pass
 
     @staticmethod
     @abstractmethod
-    def input_routing(CGRA, DFG, **info):
+    def input_routing(CGRA, in_DFG, mapping, routed_graph, **info):
         pass
 
     @staticmethod
     @abstractmethod
-    def output_routing(CGRA, DFG, **info):
+    def output_routing(CGRA, out_DFG, mapping, routed_graph, **info):
         pass
