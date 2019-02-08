ea7c487eee971d445b7cccebb97c713a4aa72c89
小島拓也 <kojimatakuya@TKMacBookAir.local>
2019-02-08 11:29:03 +0900

    impl Individual

--- Individual.py
+++ Individual.py
@@ -1,3 +1,25 @@
+import random
+import copy
 
 class Individual():
-    pass
+    def __init__(self, CGRA, init_maps = None, preg_num = None):
+        self.__model = CGRA
+        if not init_maps is None:
+            self.mapping = copy.deepcopy(init_maps[random.randint(0, len(init_maps) - 1)])
+        else:
+            self.mapping = []
+        if not preg_num is None:
+            self.preg = [random.randint(0, 1) for i in range(preg_num)]
+        else:
+            self.preg = []
+        self.routed_graph = CGRA.getNetwork()
+
+    def set_cost(self, cost):
+        self.cost = cost
+
+    def cxSet(self, other):
+        child1 = Individual(self.__model)
+        child2 = Individual(self.__model)
+
+    def mutSet(self):
+        pass
